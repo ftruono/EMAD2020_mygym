8152bb9473eec7f4b872453bcefa8453c92e031e
enzoLiguo <liguorinovincenzo0@gmail.com>
2020-11-29 18:03:14 +0100

    #21 resolution bug

--- app/screens/user/IniziaAllenamento.js
+++ app/screens/user/IniziaAllenamento.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
 import HeaderComponent from "../../component/HeaderComponent";
-import DropDown from "react-native-dropdown-picker"
 import Icon from "react-native-vector-icons/Octicons"
 import Clock from '../../component/Clock';
+import { TextInput } from 'react-native-paper';
 
 
 export default class IniziaAllenamento extends React.Component {
@@ -12,7 +12,8 @@ export default class IniziaAllenamento extends React.Component {
         super(props);
         this.state = {
             selectedValue:'',
-            index: 0
+            index: 0,
+            peso:''
         }
     }
 
@@ -48,19 +49,11 @@ export default class IniziaAllenamento extends React.Component {
                                 <Text style={[styles.textTitle,{marginTop:35,marginLeft:45}]}>Colpi: {colpi[this.state.index]}</Text>
                             </View>
                             <View style={styles.action}>
-                                <DropDown
-                                    items={[
-                                        { label: 'Scegli un peso', value: '' },
-                                        { label: '10 Kg', value: '10' },
-                                        { label: '20 Kg', value: '20' },
-                                        { label: '30 Kg', value: '30' },
-                                    ]}
-                                    placeholder="Scegli un peso"
-                                    containerStyle={{height: 40,width:200}}
-                                    style={{backgroundColor: '#fafafa'}}
-                                    onChangeItem={item => this.setState({
-                                        selectedValue: item.value
-                                    })}
+                                <TextInput
+                                    label="Inserisci i colpi"
+                                    value={this.state.peso}
+                                    onChangeText={(text) => (this.setState({peso:text}))}
+                                    //onBlur={invio i dati al db}
                                 />
                             </View>
                                 </>):(
