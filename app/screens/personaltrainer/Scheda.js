9ed90082e8cb0b0f04a265888756f673f930b16c
Nello <43914023+nellop97@users.noreply.github.com>
2020-11-29 21:30:57 +0100

    Scheda Allenamento #12 aggiunta la possibilita di selezionare un giorno per aggiungerci gli esercizi, sistemato il bug che degli input text che mostrava i valori inseriti alla riga precedenete

--- app/screens/personaltrainer/Scheda.js
+++ app/screens/personaltrainer/Scheda.js
@@ -7,47 +7,35 @@ import { Modal, Portal, Text, Button, Provider, TextInput } from 'react-native-p
 
 import DropDown from "react-native-picker-select";
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
 const { width, height } = Dimensions.get('screen');
-
-
-
 const Scheda = (props) => {
-    const [selectEsercizio, setTextEsericizio] = React.useState('');
-    const [textRipetizioni, setTextRipetizioni] = React.useState('');
-    const [textColpi, setTextColpi] = React.useState('');
-    const [textRecupero, setTextRecupero] = React.useState('');
-   
-    var schedaArray = props.scheda;
-    var index = props.index;
-    const salva=(num,es,val)=> props.aggiungiValori(schedaArray.day,num,es,val);
-    // const setIniziale=()=>{if(schedaArray.esercizi.ripetizioni!='') setTextRipetizioni(schedaArray.esercizi.ripetizioni)}
-    //     if(schedaArray.esercizi.colpi!=null)
-    //     if(schedaArray.esercizi.recupero!=null))}
-    // textRipetizioni=
-    // textColpi
-    // textRecupero;
-    
+    let scheda = props.scheda;
+    let salva = (num, es, val) => props.aggiungiValori(scheda.day, num, es, val);
+    let selectDay = (day) => (props.daySelected(day),console.log("ciao"));
+
+
+
+
 
     return (
-
-        <View style={styles.item}>
-            <Card style={{ flex: 1 }}>
-
-                <Card.Title>Day {schedaArray.day}</Card.Title>
+        <View style={styles.item} onClick={() => selectDay(scheda.day)} onPress={() => selectDay(scheda.day)}>
+            <Card style={{ flex: 1 }} >
+                <Card.Title>Day {scheda.day}</Card.Title>
                 <View >
-                    {schedaArray.esercizi.map((u, i) => {
-                        
+                    {scheda.esercizi.map((u, i) => {
                         return (
                             <View>
-                                {console.log(index)}
                                 <Text>esercizio +{i}</Text>
                                 <Card.Divider />
                                 <View style={{ flex: 1, flexDirection: 'row' }}>
                                     <DropDownPicker
-                                        onChangeItem={(value) => (setTextEsericizio(value.value), salva(i,'0',value.value))}
+                                        onChangeItem={(value) => salva(i, '0', value.value)}
                                         placeholder="Seleziona un esercizio"
 
-                                        value={selectEsercizio}
+                                        value={u.esercizio}
                                         items={[
                                             { label: 'panca piana', value: "panca_piana" },
                                             { label: 'panca alta', value: 'panta_alta' },
@@ -56,52 +44,37 @@ const Scheda = (props) => {
                                     />
                                     <TextInput
                                         label="Inserisci le ripetizioni"
-                                        value={textRipetizioni}
-                                        // style={style.textInputSyle}
-                                        onChangeText={(text) => (setTextRipetizioni(text))}
-                                        onBlur={()=>salva(i,'1',textRipetizioni)}
+                                        value={u.ripetizioni}
+                                        onChangeText={(text) => salva(i, '1', text)}
+
                                     />
                                     <TextInput
                                         label="Inserisci i colpi"
-                                        value={textColpi}
-                                        // style={style.textInputSyle}
-                                        onChangeText={(text) => (setTextColpi(text))}
-                                        onBlur={()=>salva(i,'2',textColpi)}
+                                        value={u.colpi}
+                                        onChangeText={(text) => salva(i, '2', text)}
                                     />
                                     <TextInput
                                         label="Inserisci il recupero"
-                                        value={textRecupero}
-                                        // style={style.textInputSyle}
-                                        onChangeText={(text) => (setTextRecupero(text))}
-                                        onBlur={()=>salva(i,'3',textRecupero)}
+                                        value={u.recupero}
+                                        onChangeText={(text) => salva(i, '3', text)}
                                     />
                                 </View>
-
                             </View>
-
                         );
                     })}
-
-
                 </View>
-
             </Card>
-
         </View>
-
-
-
     );
-
-
 }
 
 
 export default Scheda;
 const styles = StyleSheet.create({
-    item: {
-        width: width / 2,
 
+    item: {
+        
+    
     },
     body: {
         alignItems: 'center',
